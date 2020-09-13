607c43788ebbc0ff0bafe4eaa7816d90c4a7b600
imentu <imentu@foxmail.com>
2020-09-13 19:26:39 +0800

    login form complete.

--- app/auth/util.py
+++ app/auth/util.py
@@ -5,30 +5,30 @@ from app import db
 def validate_username_password(username, password, default_result, default_message):
     result, message = default_result, default_message
     if not username or username.strip() is '':
-        result, message = False, 'username error.'
+        result, message = False, '用户名不能为空'
     if not password or password.strip() is '':
-        result, message = False, 'password error.'
+        result, message = False, '密码不能为空'
     return result, message
 
 
 def login_verify(username, password):
-    result, message = validate_username_password(username, password, True, 'login success')
+    result, message = validate_username_password(username, password, True, '登陆成功')
     user = None
     if result:
         user = User.query.filter_by(username=username).first()
         if not user or not user.check_password(password):
-            result, message = False, 'username or password error.'
+            result, message = False, '用户名或密码错误'
             user = None
     return {'info': {'success': result, 'message': message}, 'user': user}
 
 
 def register_verify(username, password):
-    result, message = validate_username_password(username, password, True, 'register success')
+    result, message = validate_username_password(username, password, True, '注册成功')
     user = None
     if result:
         user = User.query.filter_by(username=username).first()
         if user:
-            result, message = False, 'username already exists.'
+            result, message = False, '该用户名已存在'
             user = None
         else:
             user = User(username=username, password=password)
