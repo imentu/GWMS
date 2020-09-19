f90b988b853f7a325041814d91ff4ac2eeb2239a
imentu <imentu@foxmail.com>
2020-09-19 09:32:11 +0800

    three auth tests.

--- test_all.py
+++ test_all.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from app import create_app
@@ -20,10 +22,49 @@ def login(client, username, password):
     ), follow_redirects=True)
 
 
+def register(client, username, password, name='', gender=0, college='', major='', status='', employment=''):
+    return client.post('/auth/register', data=dict(
+        username=username,
+        password=password,
+        name=name,
+        gender=gender,
+        college=college,
+        major=major,
+        status=status,
+        employment=employment
+    ), follow_redirects=True)
+
+
 def logout(client):
     return client.get('/auth/logout', follow_redirects=True)
 
 
+def test_logout_without_login(client):
+    r = logout(client)
+    assert r.status_code == 401
+
+
 def test_login_logout(client):
-    rv = login(client, 'admin', 'admin')
-    assert b'"success": true' in rv.data
+    r = login(client, 'admin', 'admin')
+    j = r.get_json()
+    assert j['success'] is True
+
+    r = logout(client)
+    j = r.get_json()
+    assert j['success'] is True
+
+
+def test_register(client):
+    username = os.urandom(16)
+    password = os.urandom(16)
+    r = register(client, username, password)
+    j = r.get_json()
+    assert j['success'] is True
+
+    r = login(client, username, password)
+    j = r.get_json()
+    assert j['success'] is True
+
+    r = logout(client)
+    j = r.get_json()
+    assert j['success'] is True
